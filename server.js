bf7753db2f78b02dd8384b82e591348167c8551e
Romeo <romeocorpuz808@gmail.com>
2018-08-09 19:31:50 -1000

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+
+const net = require('net');
+const users = [];
+
+let server = net.createServer((socket) => {
+
+  socket.setEncoding('utf8');
+  // console.log(socket.remotePort);
+  server.getConnections(function(count){
+    console.log(`${socket.remotePort} connected. There are ${count} users chatting.`);
+  });
+
+  users.push(socket);
+
+  socket.on('data', function (data) {
+    process.stdout.write(`${socket.remotePort}: ${data}`);
+    users.forEach(function(eachUser){
+      eachUser.write(`${socket.remotePort}: ${data}`);
+    });
+  });
+
+  process.stdin.on('data', (cmd)=>{
+    users.forEach(function(eachUser){
+      eachUser.write(`[admin]: ${cmd}`);
+    });
+  });
+
+  socket.on('end', ()=>{
+    server.getConnections(function(count){
+      console.log(`${socket.remotePort} disconnected. There are ${count} users chatting.`);
+    });
+  });
+});
+
+server.listen(6969, '0.0.0.0', ()=>{
+  console.log(`opened server on`, server.address());
+});
